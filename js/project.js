5ae38f40dd3c575a730460a965102c35608c720b
Fushigo <rizky23ska@gmail.com>
2023-09-08 17:22:11 +0700

    new update

--- js/project.js
+++ js/project.js
@@ -13,36 +13,3 @@ function openProject(evt, projectName) {
   evt.currentTarget.className += " activeNav";
 }
 document.getElementById("default").click();
-<<<<<<< HEAD
-=======
-
-// Button Config
-document.getElementById("glatz").addEventListener("click", function () {
-  var websiteURL = "https://glatzauto.com";
-  window.open(websiteURL, "_blank");
-});
-document.getElementById("dai").addEventListener("click", function () {
-  var websiteURL = "https://digitalakademiindonesia.com";
-  window.open(websiteURL, "_blank");
-});
-document.getElementById("madani").addEventListener("click", function () {
-  var websiteURL = "https://madaniplast.com";
-  window.open(websiteURL, "_blank");
-});
-document.getElementById("ajak").addEventListener("click", function () {
-  var websiteURL = "https://ajakonline.com/kelasmagang";
-  window.open(websiteURL, "_blank");
-});
-document.getElementById("chandani").addEventListener("click", function () {
-  var websiteURL = "https://batikchandani.com";
-  window.open(websiteURL, "_blank");
-});
-document.getElementById("vandel").addEventListener("click", function () {
-  var websiteURL = "https://griyavandel.com";
-  window.open(websiteURL, "_blank");
-});
-document.getElementById("btn-project").addEventListener("click", function () {
-  var websiteURL = "../page/project.html";
-  window.open(websiteURL, "_blank");
-});
->>>>>>> e51f4b0f6a6cda0fb222963b29af9ea87f64e42f
